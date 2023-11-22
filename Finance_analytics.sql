ab6ed6f8a0fad2896c326f56f98350931b936cf4
Sumit Malviya <143081763+SumitMgithub@users.noreply.github.com>
2023-11-22 20:14:36 +0530

    Update Finance_analytics.sql

--- Finance_analytics.sql
+++ Finance_analytics.sql
@@ -1,29 +1,19 @@
--- Finance Analytics
+-- FINANCE ANALYTICS
 
--- a. Get customer code for Croma india
-	SELECT * FROM dim_customer 
-    WHERE customer like "%croma%" AND market="india";
 
--- b. Get all the sales transaction data from fact_sales_monthly table for that customer(croma: 90002002) in the fiscal_year 2021
-	SELECT * FROM fact_sales_monthly 
-	WHERE 
-            customer_code=90002002 AND
-            YEAR(DATE_ADD(date, INTERVAL 4 MONTH))=2021 
-	ORDER BY date asc
-	LIMIT 100000;
+-- Create a function 'get_fiscal_year' to get the fiscal year by passing the date
 
--- c. create a function 'get_fiscal_year' to get fiscal year by passing the date
 	CREATE FUNCTION `get_fiscal_year`(calendar_date DATE) 
 	RETURNS int
     	DETERMINISTIC
 	BEGIN
-        	DECLARE fiscal_year INT;
-        	SET fiscal_year = YEAR(DATE_ADD(calendar_date, INTERVAL 4 MONTH));
-        	RETURN fiscal_year;
+		DECLARE fiscal_year INT;
+		SET fiscal_year = YEAR(DATE_ADD(calendar_date, INTERVAL 4 MONTH));
+		RETURN fiscal_year;
 	END
 
 
-### Create a report of individual product sales (aggregated on a monthly basis at the product code level) for Chroma India customer for FY = 2021
+### Create a report of individual product sales (aggregated on a monthly basis at the product code level) for Chroma India customers for FY = 2021
 
 	SELECT 
     	    s.date, s.product_code, 
@@ -75,9 +65,10 @@
 	END
 
 
-### Stored Procedure: To determine Market Badge
+### Stored Procedure: To determine the Market Badge
 
---  Write a stored procedure that can determine market badge. i.e. if total sold quantity > 5 million that market is considered "Gold" else "Silver"
+--  Write a stored procedure that can determine the market badge. i.e. if the total sold quantity > 5 million that market is considered "Gold" else "Silver"
+		
 	CREATE PROCEDURE `get_market_badge`(
         	IN in_market VARCHAR(20),
         	IN in_fiscal_year YEAR,
@@ -111,59 +102,59 @@
 	END
     
     
-### Write a stored procedure for getting top n products in each division by their quantities sold in a given FY
+### Write a stored procedure for getting the top n products in each division by their quantities sold in a given FY
 
-		CREATE PROCEDURE `get_top_n_products_per_division_by_qty_sold` (
-		in_fiscal_year INT,
-		in_top_n INT
-		)
-		BEGIN
-			WITH 
-			cte1 AS 
-				(
-					SELECT 
-						p.division,
-						p.product,
-						sum(sold_quantity) as total_qty
-					FROM dim_product p
-					JOIN fact_sales_monthly s
-					ON p.product_code = s.product_code
-					WHERE fiscal_year=in_fiscal_year
-					GROUP BY p.product, p.division
-				),
-			cte2 AS
-				(
-					SELECT 
-						*,
-						dense_rank() over(partition by division order by total_qty desc) as d_rnk
-					FROM cte1
-				)
+	CREATE PROCEDURE `get_top_n_products_per_division_by_qty_sold` (
+	in_fiscal_year INT,
+	in_top_n INT
+	)
+	BEGIN
+		WITH 
+		cte1 AS 
+			(
 			SELECT 
-				* 
-			FROM cte2
-			WHERE d_rnk <=in_top_n;
-		END
+				p.division,
+				p.product,
+				sum(sold_quantity) as total_qty
+			FROM dim_product p
+			JOIN fact_sales_monthly s
+			ON p.product_code = s.product_code
+			WHERE fiscal_year=in_fiscal_year
+			GROUP BY p.product, p.division
+			),
+		cte2 AS
+			(
+			SELECT 
+				*,
+				dense_rank() over(partition by division order by total_qty desc) as d_rnk
+			FROM cte1
+			)
+		SELECT 
+			* 
+		FROM cte2
+		WHERE d_rnk <=in_top_n;
+	END
 	
 
-### Retrieve top 2 markets in every region by their gross sales amount in financial year 2021
+### Retrieve the top 2 markets in every region by their gross sales amount in financial year 2021
 		
-		WITH
-		cte1 AS
-	    (
-			SELECT 
-				c.market,
-				c.region,
-				ROUND(SUM(gross_price_total/1000000),2) as gross_price_mn
-			FROM dim_customer c
-			JOIN gross_sales s
-			ON c.customer_code = s.customer_code
-			GROUP BY c.region,c.market
-		),
-		cte2 AS
-	    (
-			SELECT
-				*,
-				dense_rank() over(partition by region order by gross_price_mn desc) AS rnk
-			FROM cte1
-		)
-		SELECT * FROM cte2 WHERE rnk<=2;
+	WITH
+	cte1 AS
+   	(
+		SELECT 
+			c.market,
+			c.region,
+			ROUND(SUM(gross_price_total/1000000),2) as gross_price_mn
+		FROM dim_customer c
+		JOIN gross_sales s
+		ON c.customer_code = s.customer_code
+		GROUP BY c.region,c.market
+	),
+	cte2 AS
+   	(
+		SELECT
+			*,
+			dense_rank() over(partition by region order by gross_price_mn desc) AS rnk
+		FROM cte1
+	)
+	SELECT * FROM cte2 WHERE rnk<=2;
